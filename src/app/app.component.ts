02a4e580e90bffb1fea0b7b8e900b6d8b9c0e829
John Paxton <johnpaxton@gmail.com>
2024-06-25 03:33:39 -0400

    Dynamically generate labels and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { UIRouterModule } from '@uirouter/angular';
+import { routerStates } from './app.routing';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 	standalone: true,
-	imports: [UIRouterModule],
+	imports: [UIRouterModule, CommonModule],
 })
 export class AppComponent {
 	title = 'angular-demos';
+	protected readonly routerStates = routerStates;
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,20 +2,24 @@ import { Route01Component } from './route01.component';
 import { Route02Component } from './route02.component';
 import { Route03Component } from './route03.component';
 
+// Technically the type is Ng2StateDeclaration[]
 export const routerStates = [
 	{
-		name: 'route01',
-		url: '/route01',
+		name: 'route-01',
+		url: 'route01',
 		component: Route01Component,
+		label: 'Route 1',
 	},
 	{
-		name: 'route02',
-		url: '/route02',
+		name: 'route-02',
+		url: 'route02',
 		component: Route02Component,
+		label: 'Route 2',
 	},
 	{
-		name: 'route03',
-		url: '/route03',
+		name: 'route-03',
+		url: 'route03',
 		component: Route03Component,
+		label: 'Route 3',
 	},
 ];
